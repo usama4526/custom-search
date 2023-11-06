d3b5a570435c8b55b60878c517866dbfab14f79d
usama4526 <wusama19@gmail.com>
2023-11-07 02:04:47 +0500

    controller change

--- controllers/siteScraperController.js
+++ controllers/siteScraperController.js
@@ -3,20 +3,18 @@ const scrapeVideoUrls = require('../scraper/videoScraper')
 exports.getSiteVideoSrc = async (req, res) => {
     console.log('posted url', req.body.url);
     try {
-        const videoUrls = await scrapeVideoUrls(req.body.url);
+        //const videoUrls = await scrapeVideoUrls(req.body.url);
         console.log('response from function', videoUrls);
-        res.status(200).json({
-            status: 'success',
-            results: videoUrls.length,
-            data: {
-                videoUrls: videoUrls
-            }
-        });
+        scrapeVideoUrls(req.body.url).then(videoUrls=>{
+            res.status(200).json({
+                status: 'success',
+                results: videoUrls.length,
+                data: {
+                    videoUrls: videoUrls
+                }
+            });
+        })
     } catch (error) {
-        console.error('An error occurred:', error);
-        res.status(500).json({
-            status: 'error',
-            message: 'Internal server error'
-        });
+
     }
 }
