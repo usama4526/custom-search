4084ccf85432b8f6d4c6944ce232073fa53a6a40
usama4526 <wusama19@gmail.com>
2023-11-12 22:02:15 +0500

    launch timeout

--- scraper/videoScraper.js
+++ scraper/videoScraper.js
@@ -8,18 +8,20 @@ async function scrapeVideoUrls(url) {
   defaultViewport: null,
   ignoreHTTPSErrors: true,
   headless: true,
-  executablePath:process.env.PUPPETEER_EXECUTABLE_PATH,
-  waitForInitialPage: true
+  executablePath:process.env.PUPPETEER_EXECUTABLE_PATH
   })
   const page = await browser.newPage()
  // await page.setViewport({ width: 1920, height: 1080 });
   await page.setJavaScriptEnabled(true);
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+  //await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
 
-  await page.goto(url,{timeout:0})
+  await page.goto(url)
     const title = await page.title()
     console.log('page title',title);
+  await page.screenshot({path:'example.png'})
   await page.waitForSelector('video',{timeout:0})
+  const html = await page.content()
+  console.log('pagehtml',html);
   const videoUrls = await page.evaluate(() => {
     const videoElements = Array.from(document.querySelectorAll('video source, video'));
     return videoElements.map(element => {
@@ -31,6 +33,7 @@ async function scrapeVideoUrls(url) {
       return null; // handle other cases if needed
     }).filter(src => src !== null);
   });
+
   
   console.log('videoUrls',videoUrls);
   
