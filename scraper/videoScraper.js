9445207d9a4ecc894b4f0d63e16ee2f04d764d27
usama4526 <wusama19@gmail.com>
2023-11-23 12:36:45 +0500

    timeout duration set

--- scraper/videoScraper.js
+++ scraper/videoScraper.js
@@ -8,18 +8,19 @@ async function scrapeVideoUrls(url) {
   defaultViewport: null,
   ignoreHTTPSErrors: true,
   headless: true,
-  executablePath:process.env.PUPPETEER_EXECUTABLE_PATH
+  executablePath:process.env.PUPPETEER_EXECUTABLE_PATH,
+  waitForInitialPage: true
   })
   const page = await browser.newPage()
  // await page.setViewport({ width: 1920, height: 1080 });
   await page.setJavaScriptEnabled(true);
   //await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
 
-  await page.goto(url)
+  await page.goto(url,{timeout:10000})
     const title = await page.title()
     console.log('page title',title);
   await page.screenshot({path:'example.png'})
-  await page.waitForSelector('video',{timeout:0})
+  await page.waitForSelector('video',{timeout:10000})
   const html = await page.content()
   console.log('pagehtml',html);
   const videoUrls = await page.evaluate(() => {
